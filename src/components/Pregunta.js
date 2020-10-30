13352e04ac59e9af3918608380a346698012d86a
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2020-10-30 12:39:02 -0400

    se creo el state para definir el presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,6 +1,25 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState} from 'react';
 
 const Pregunta = () => {
+    
+    // Definir el state
+
+    const [cantidad, guardarCantidad] = useState(0);
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value,10))
+    }
+
+    // Submit para definir el presupuesto
+
+    const agregarPresupuesto = e =>{
+        e.preventDefault();
+
+        // Validar
+
+        // Si se pasa lña validacion
+    }
+
     return (  
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
@@ -9,6 +28,7 @@ const Pregunta = () => {
                     type="number"
                     className = "u-full-width"
                     placeholder = "Coloca tu presupuesto"
+                    onChange = {definirPresupuesto}
                 />
 
                 <input 
